d4a4f57ee027adf3216d6fa591aa9ac6dacc1404
Fada Arzimanli <fada.arzimanli@gmail.com>
2022-10-31 03:00:27 +0400

    design updated

--- js/script.js
+++ js/script.js
@@ -1,26 +1,56 @@
 $(window).on('load', function () {
 
-    // Scroll top button
-    // $offset = $("#header").outerHeight();
-    // $(window).scroll(function () {
-    //     if ($(this).scrollTop() > 200)
-    //         $('#btn-scroll-top').fadeIn(200);
-    //     else
-    //         $('#btn-scroll-top').fadeOut(200);
+    // Preloader
+    $('.preloader img').fadeOut();
+    $('.preloader').delay(200).fadeOut();
+    $('body').removeClass("overflow-hidden");
 
-    //     if ($(this).scrollTop() > $offset + 200) {
-    //         $("#header").addClass("fixed");
-    //         $('body').css("padding-top", $offset + "px");
-    //     }
-    //     else {
-    //         $("#header").removeClass("fixed");
-    //         $('body').css("padding-top", "0");
-    //     }
-    // });
+    // Scroll top button
+    $offset = $("#header").outerHeight();
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 200)
+            $('#btn-scroll-top').fadeIn(200);
+        else
+            $('#btn-scroll-top').fadeOut(200);
+
+        if ($(this).scrollTop() > $offset + 200) {
+            $("#header").addClass("fixed");
+        }
+        else {
+            $("#header").removeClass("fixed");
+        }
+    });
 
 });
 
 $(function () {
+    // Document click
+    $(document).mouseup(function (e) {
+        var langList = $(".lang-list");
+
+        if (!langList.is(e.target) && langList.has(e.target).length === 0) {
+            langList.find('button').stop(true, false, true).removeClass('active');
+            $('.lang-dropdown').stop(true, false, true).removeClass('active');
+        }
+    });
+    // Scroll top button
+    $('#btn-scroll-top').click(function () {
+        $("html, body").animate({ scrollTop: 0 }, 0);
+    });
+
+    // Go to
+    $('.go-to').click(function (e) {
+        e.preventDefault()
+        let fixedHeight = $('#header').outerHeight();
+        let id = $(this).attr("href");
+        $("html, body").animate({ scrollTop: $(id).offset().top - fixedHeight }, 0);
+    });
+
+    // Lang list
+    $('.lang-list button').click(function () {
+        $(this).stop(true, false, true).toggleClass('active');
+        $('.lang-dropdown').stop(true, false, true).toggleClass('active');
+    });
 
     $('.accardion-item-header button').click(function () {
         const accordionBody = $(this).closest('.accardion-item').find('.accardion-item-body');
@@ -31,15 +61,15 @@ $(function () {
 
     // Welcome swiper
     const welcomeSwiper = new Swiper(".welcomeSwiper", {
-        spaceBetween: 50,
+        spaceBetween: 0,
         autoplay: {
-            pauseOnMouseEnter: false,
+            pauseOnMouseEnter: true,
             disableOnInteraction: false,
             delay: 3000,
         },
         pagination: {
             el: ".swiper-pagination",
-            type: "progressbar",
+            clickable: true
         },
         keyboard: {
             enabled: true,
@@ -52,7 +82,7 @@ $(function () {
         slidesPerView: 7,
         spaceBetween: 100,
         autoplay: {
-            pauseOnMouseEnter: false,
+            pauseOnMouseEnter: true,
             disableOnInteraction: false,
             delay: 3000,
         },
@@ -61,9 +91,9 @@ $(function () {
     // Products swiper
     const productsSwiper = new Swiper(".productsSwiper", {
         slidesPerView: 4,
-        spaceBetween: 60,
+        spaceBetween: 32,
         autoplay: {
-            pauseOnMouseEnter: false,
+            pauseOnMouseEnter: true,
             disableOnInteraction: false,
             delay: 3000,
         },
@@ -76,7 +106,7 @@ $(function () {
     // Sub services swiper
     const subServicesSwiper = new Swiper(".subServicesSwiper", {
         slidesPerView: 4,
-        spaceBetween: 120,
+        spaceBetween: 100,
         autoplay: {
             pauseOnMouseEnter: false,
             disableOnInteraction: false,
@@ -87,7 +117,7 @@ $(function () {
     // Products swiper
     const serviceCenterSwiper = new Swiper(".serviceCenterSwiper", {
         slidesPerView: 3,
-        spaceBetween: 44,
+        spaceBetween: 36,
         autoplay: {
             pauseOnMouseEnter: false,
             disableOnInteraction: false,
@@ -99,7 +129,42 @@ $(function () {
         },
     });
 
+    // Products gallery swiper
+    const productGallerySwiper = new Swiper(".productGallerySwiper", {
+        spaceBetween: 0,
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+    });
+
+    // Blogs swiper
+    const blogsSwiper = new Swiper(".blogsSwiper", {
+        slidesPerView: 3,
+        spaceBetween: 68,
+        autoplay: {
+            pauseOnMouseEnter: true,
+            disableOnInteraction: false,
+            delay: 3000,
+        }
+    });
+
+    // Fancybox
     Fancybox.bind("[data-fancybox]", {
         // Your options go here
     });
+
+    // Copy text
+    $(".copy-to-clipboard").click(function () {
+        $(this).find("span").fadeIn(150).delay(300).fadeOut(150);
+        let $temp = $("<input>");
+        $("body").append($temp);
+        $temp.val($(this).data('href')).select();
+        document.execCommand("copy");
+        $temp.remove();
+    })
 })
